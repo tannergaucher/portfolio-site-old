646af4054d85bebfab1cbdd8036340caf194425f
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2019-11-04 14:28:21 +0700

    add border to primary btn

--- site/src/components/styles/button.js
+++ site/src/components/styles/button.js
@@ -1,25 +1,19 @@
 import styled from "styled-components"
 
 const Button = styled.button`
-  padding: 0.5rem 1rem;
-  border: 2px solid black;
-  border-radius: var(--radius);
   font-size: var(--font-size-lg);
-  /* change to CSS VARS FOR DARK MODE */
+  padding: 0.5rem 1rem;
+  border-radius: var(--radius);
   background: ${props => (props.primary ? "var(--btn-primary)" : "var(--btn)")};
-  border-color: var(--btn-text);
+  border: 2px solid var(--btn-border);
   color: ${props =>
     props.primary ? "var(--btn-primary-text)" : "var(--btn-text)"};
   text-transform: uppercase;
-  transition-duration: 0.2s;
-  
+  /* transition-duration: 0.2s; */
+
   &:hover {
     /* transition-duration: 0.2s; */
-    /* background: ${props => (props.primary ? "inherit" : "black")};
-    color: ${props => (props.primary ? "blue" : "white")}; */
   }
-
-  
 
   @media (max-width: 768px) {
     font-size: var(--font-size-sm);
