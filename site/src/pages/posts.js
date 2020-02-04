d3cb503c709bb25fa51ba71a9cd938bdba3dcbd7
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2020-02-04 09:31:57 -0500

    Center posts and projects pages

--- site/src/pages/posts.js
+++ site/src/pages/posts.js
@@ -2,17 +2,24 @@ import { Layout, SEO } from "../components/elements"
 
 import { PostCard } from "../components/post"
 import React from "react"
+import styled from "styled-components"
 import { useAllPosts } from "../components/hooks"
+
+const StyledPostsPage = styled.div`
+  margin: var(--space-xl) auto;
+  max-width: var(--container);
+`
 
 export default function PostsPage({ location }) {
   const { edges } = useAllPosts()
 
   return (
-    <Layout location={location}>
-      <SEO title="Posts" />
-      {edges.map(edge => (
-        <PostCard key={edge.node.id} post={edge.node} />
-      ))}
+    <Layout location={location} noContainer="true">
+      <StyledPostsPage>
+        {edges.map(edge => (
+          <PostCard key={edge.node.id} post={edge.node} />
+        ))}
+      </StyledPostsPage>
     </Layout>
   )
 }
--- site/src/pages/projects.js
+++ site/src/pages/projects.js
@@ -2,17 +2,25 @@ import { Layout, SEO } from "../components/elements"
 
 import { ProjectCard } from "../components/project"
 import React from "react"
+import styled from "styled-components"
 import { useProjects } from "../components/hooks"
+
+const StyledProjectsPage = styled.div`
+  max-width: var(--container);
+  margin: var(--space-xl) auto;
+`
 
 export default function Projects({ location }) {
   const { edges } = useProjects()
 
   return (
-    <Layout location={location}>
-      <SEO title="Projects" />
-      {edges.map(edge => (
-        <ProjectCard key={edge.node.id} project={edge.node} />
-      ))}
+    <Layout location={location} noContainer="true">
+      <StyledProjectsPage>
+        <SEO title="Projects" />
+        {edges.map(edge => (
+          <ProjectCard key={edge.node.id} project={edge.node} />
+        ))}
+      </StyledProjectsPage>
     </Layout>
   )
 }
